0a8abe8029e0685c9d0d790e405b45100d6c1e1f
cjoelrun <cameronjlopez@gmail.com>
2014-01-24 13:38:31 -0600

    working crawler

--- consume.py
+++ consume.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python
 
 import argh
+import xmltodict
 from ampq import AmqpConnection
+from vcd import VCloud
+
+
+def resolve_entity(data):
+    links = data['Entity']['Link']
+    if isinstance(links, list):
+        for link in links:
+            resolve(link['@href'], link['@type'])
+    else:
+        resolve(links['@href'], links['@type'])
+
+
+def resolve(url, name):
+    vcloud = VCloud()
+    print name
+    response = vcloud.req(url)
+    data = xmltodict.parse(response.text)
+    # print response.text
+    if "Entity" in data:
+        resolve_entity(data)
+    else:
+        print response.text
 
 
 def callback(channel, method, header, body):
+    data = xmltodict.parse(body)
+    print "{0}:".format(data['vmext:Notification']['@type'])
     print body
+    resolver = data['vmext:Notification']['vmext:Link']['@href']
+    for entity in data['vmext:Notification']['vmext:EntityLink']:
+        name = entity['@type']
+        id = entity['@id']
+        resolve(resolver+id, name)
 
 
 def consume(host, username, password, virtual_host, queue):
